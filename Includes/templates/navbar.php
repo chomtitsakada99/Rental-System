04507fa5dacae094409b6e979bd4dc661fbb82e8
chom titsakada <chomtitsakada999@gmail.com>
2026-02-04 15:12:03 +0700

    final Project Team website php

---
+++ Includes/templates/navbar.php
@@ -0,0 +1,33 @@
+<!-- START NAVBAR SECTION -->
+<header  id="header" class="nav-header">
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class = "container">
+            <a class="navbar-brand" href="#">
+                CarRental<span style = "color:#04DBC0">system</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#yahyaNavbar" aria-controls="yahyaNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="yahyaNavbar">
+                <ul class="navbar-nav" style = "margin-left: auto!important;">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="./">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./#services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./#brands">Brands</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./#reserve">Reserve</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./#contact-us">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
